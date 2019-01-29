8ef149a145d4193cf2ce4a2fc16ebe9f1bbb07e7
khansamad99 <37273365+khansamad99@users.noreply.github.com>
2019-01-29 23:21:14 +0530

    Add files via upload

---
+++ search.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+	// body...
+	$('#search').keyup(function(){
+		var searchValue = ($('#search').val()).trim();
+		if(searchValue!='')
+			$.getJSON('list.json',function(result){
+				var list = result.list;
+			    var options = {
+					  shouldSort: false,
+					  threshold: 0.6,
+					  location: 0,
+					  distance: 100,
+					  maxPatternLength: 32,
+					  minMatchCharLength: 2,
+					  keys: [
+					    "title", 
+					    "author",
+					    "year"
+					]
+					};
+					var fuse = new Fuse(list, options); // "list" is the item array
+					var searchresult = fuse.search(searchValue);
+					console.log(searchresult);
+					if(searchresult.length > 0)
+					{
+						$('#results').empty();
+						for(i=0;i<searchresult.length;i++){
+							$('#results').append('<div class="res"><h3>'+'<a target="_blank" class="result-link" href="'+searchresult[i].Link+'">'+searchresult[i].title+' / '+searchresult[i].author+  '/'+ searchresult[i].year + ' </p></div>');	
+						}
+					}
+					
+	})
+})})
